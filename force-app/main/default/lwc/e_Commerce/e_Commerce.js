e56ef351972cb771197f5c47dbf27ae20d224b07
siddharthjain574 <jainsiddharth975@gmail.com>
2023-08-09 11:51:33 +0530

    E-Commerce final commit

--- force-app/main/default/lwc/e_Commerce/e_Commerce.js
+++ force-app/main/default/lwc/e_Commerce/e_Commerce.js
@@ -1,3 +1,4 @@
+
 import { LightningElement, track, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import getProducts from '@salesforce/apex/AddProduct.getProducts';
@@ -8,68 +9,75 @@ import ACCOUNT_FIELD from '@salesforce/schema/Contract.AccountId';
 import CONTRACT_END_DATE_FIELD from '@salesforce/schema/Contract.EndDate';
 import CONTRACT_START_DATE_FIELD from '@salesforce/schema/Contract.StartDate';
 
-// PRODUCT DATATABLE COLUMNS
+
+import ACCOUNT_NAME_FIELD from '@salesforce/schema/Account.Name';
+import AUTHORIZED_BY_ID from '@salesforce/schema/Contact.Name';
+import CONTRACT_NUMBER_FIELD from '@salesforce/schema/Contract.ContractNumber';
+import PRICEBOOK_NAME_FIELD from '@salesforce/schema/Pricebook2.Name';
+
+
+// Define the columns for the product datatable
 const columns = [
-    { label: 'Product Name', fieldName: 'Name', type: 'text' },
-    { label: 'Product Code', fieldName: 'ProductCode', type: 'text' },
-    { label: 'List Price', fieldName: 'UnitPrice', type: 'currency' },
-    { label: 'Product Description', fieldName: 'Description', type: 'text' },
-    { label: 'Product Family', fieldName: 'Family', type: 'text' }
-    
+  { label: 'Product Name', fieldName: 'Name', type: 'text' },
+  { label: 'Product Code', fieldName: 'ProductCode', type: 'text' },
+  { label: 'List Price', fieldName: 'UnitPrice', type: 'currency' },
+  { label: 'Product Description', fieldName: 'Description', type: 'text' },
+  { label: 'Product Family', fieldName: 'Family', type: 'text' }
+
 ];
 
-// SELECTED PRODUCT DATATABLE COLUMNS
+// Define the columns for the selected product datatable
 const selectedColumns = [
-    { label: 'Product', fieldName: 'Name', type: 'text'},
-    { label: 'Quantity', fieldName: 'Quantity', type: 'number', editable: true  },
-    { label: 'Unit Price', fieldName: 'UnitPrice', type: 'currency'},
-    { label: 'List Price', fieldName: 'UnitPrice', type: 'currency' },
-    { label: 'Line Description', fieldName: 'Description', type: 'text', editable: true }
+  { label: 'Product', fieldName: 'Name', type: 'text' },
+  { label: 'Quantity', fieldName: 'Quantity', type: 'number', editable: true },
+  { label: 'Unit Price', fieldName: 'UnitPrice', type: 'currency' },
+  { label: 'List Price', fieldName: 'UnitPrice', type: 'currency' },
+  { label: 'Line Description', fieldName: 'Description', type: 'text', editable: true }
 ];
 
+// Define the columns for the final selected product datatable
 const finalSelectedColumns = [
-    { label: 'Product Name', fieldName: 'Name', type: 'text'},
-    { label: 'Product Code', fieldName: 'ProductCode', type: 'text' },
-    { label: 'Unit Price', fieldName: 'UnitPrice', type: 'currency'},
-    { label: 'Quantity', fieldName: 'Quantity', type: 'number'},
-    { label: 'Total Price', fieldName: 'TotalPrice', type: 'currency'},
-    { label: 'Price Book Entry', fieldName: 'PriceBookEntryId', type: 'text' }
+  { label: 'Product Name', fieldName: 'Name', type: 'text' },
+  { label: 'Product Code', fieldName: 'ProductCode', type: 'text' },
+  { label: 'Unit Price', fieldName: 'UnitPrice', type: 'currency' },
+  { label: 'Quantity', fieldName: 'Quantity', type: 'number' },
+  { label: 'Total Price', fieldName: 'TotalPrice', type: 'currency' },
+  { label: 'Price Book Entry', fieldName: 'PriceBookEntryId', type: 'text' }
 
 ];
 
 export default class E_Commerce extends LightningElement {
 
-   //....................................................................HTML PARAMETERS...............................................................................................
-
+// Define the component's state variables
   @track order = {
     Status: 'Draft'
   };
-  accNameOptions=[];
-  contractNumberOptions=[];
-  contactNameOptions=[];
-  statusOptions=[];
 
+  // Define variables to control the visibility of different sections
   @track showOrderCard = true;
   @track showProductCard = false;
+  @track showSelectedProduct = false;
+  @track showPreviewCard = false;
+  @track showEnteredOrderDetailsCard = false;
 
+// datatable's data and column
   columns = columns;
   products;
-  
+
   selectedColumns = selectedColumns;
   @track selectedProducts = [];
 
-  @track showSelectedProduct = false;
 
-  @track showPreviewCard = false;
   @track finalSelectedProducts = [];
   finalSelectedColumns = finalSelectedColumns;
 
+//other variables
+  accNameOptions = [];
+  contractNumberOptions = [];
+  contactNameOptions = [];
+  statusOptions = [];
   @track draftValues = [];
-
   @track totalPriceSum = 0;
-
-  @track showEnteredOrderDetailsCard = false;
-
   @track dateError;
   @track contractError;
   //.......................................................................SOURCE CODE...............................................................................................
@@ -81,12 +89,13 @@ export default class E_Commerce extends LightningElement {
 
   // ORDER CARD HANDLE CHANGE ACTION PERFORM
 
-  handleChange(event){
+  handleChange(event) {
     let fieldName = event.target.fieldName;
     let value = event.target.value;
     this.order[fieldName] = value;
   }
 
+  // clear error message 
   clearErrors() {
     this.dateError = '';
     this.contractError = '';
@@ -114,7 +123,6 @@ export default class E_Commerce extends LightningElement {
     const contractStartDate = getFieldValue(this.contract.data, CONTRACT_START_DATE_FIELD);
     const contractEndDate = getFieldValue(this.contract.data, CONTRACT_END_DATE_FIELD);
     if (contractStartDate && contractEndDate && (this.order.EffectiveDate < contractStartDate || this.order.EffectiveDate > contractEndDate)) {
-      // this.showToast('Error', 'Order Start Date must be within the Contract Start Date and End Date range', 'error');
       this.dateError = 'Order Start Date should be within the contract period';
 
       return;
@@ -128,7 +136,7 @@ export default class E_Commerce extends LightningElement {
 
   // SHOW/HIDE BUTTON ACTION PERFORM
   // PREVIOUS(SHOW ORDER CARD ON CLICK)
-  handlePreviousClick(){
+  handlePreviousClick() {
     this.showOrderCard = true;
     this.showProductCard = false;
   }
@@ -138,21 +146,21 @@ export default class E_Commerce extends LightningElement {
   wiredProducts({ error, data }) {
     if (data) {
       this.products = data.map(productWrapper => ({
-          ...productWrapper.product,
+        ...productWrapper.product,
 
-      UnitPrice: productWrapper.pricebookEntries.length > 0
-        ? productWrapper.pricebookEntries[0].UnitPrice
-        : null,
+        UnitPrice: productWrapper.pricebookEntries.length > 0
+          ? productWrapper.pricebookEntries[0].UnitPrice
+          : null,
 
-      Quantity: 0,
-        
-      PriceBookEntryId: productWrapper.pricebookEntries.length > 0
-        ? productWrapper.pricebookEntries[0].Id
-        : null,
+        Quantity: 0,
 
-      PriceBookEntry: productWrapper.pricebookEntries.length > 0
-        ? productWrapper.pricebookEntries[0]
-        : null
+        PriceBookEntryId: productWrapper.pricebookEntries.length > 0
+          ? productWrapper.pricebookEntries[0].Id
+          : null,
+
+        PriceBookEntry: productWrapper.pricebookEntries.length > 0
+          ? productWrapper.pricebookEntries[0]
+          : null
       }));
     } else if (error) {
 
@@ -161,7 +169,7 @@ export default class E_Commerce extends LightningElement {
 
   // SHOW/HIDE BUTTON ACTION PERFORM
   // (SHOW SELECTED PRODUCT IN DATATABLE ON BUTTON CLICK)
-  handleAddProducts(){
+  handleAddProducts() {
 
     const selectedRows = this.template.querySelector('lightning-datatable').getSelectedRows();
     const updatedSelectedProducts = [...this.selectedProducts, ...selectedRows];
@@ -181,7 +189,7 @@ export default class E_Commerce extends LightningElement {
 
   // SHOW/HIDE BUTTON ACTION PERFORM
   // SHOW ADD PRODUCT PAGE ON BUTTON CLICK
-  handleBackClick(){
+  handleBackClick() {
     this.showSelectedProduct = false;
     this.showProductCard = true;
     this.showPreviewCard = false;
@@ -189,11 +197,7 @@ export default class E_Commerce extends LightningElement {
 
   // SHOW/HIDE BUTTON ACTION PERFORM
   // SHOW ADD PREVIEW PAGE ON BUTTON CLICK
-  handlePreviewProducts(event){
-
-    // if (!this.validateSelectedProducts()) {
-    //   return;
-    // }
+  handlePreviewProducts(event) {
 
     this.showSelectedProduct = false;
     this.showPreviewCard = true;
@@ -201,19 +205,12 @@ export default class E_Commerce extends LightningElement {
     let drafts = event.detail.draftValues;
     let draftMap = {};
 
-    drafts.forEach(draft =>{
-        draftMap[draft.Id] = draft.Quantity;
+    drafts.forEach(draft => {
+      draftMap[draft.Id] = draft.Quantity;
     });
 
-
-    // let hasZeroQuantity = this.selectedProducts.some(product => draftMap[product.Id] <= 0);
-    // if (hasZeroQuantity) {
-    //   this.showToast('Error', 'Quantity value must be greater than 0 for all selected products', 'error');
-    //   return;
-    // }
-
-    this.selectedProducts.forEach(product =>{
-        product.Quantity = draftMap[product.Id];
+    this.selectedProducts.forEach(product => {
+      product.Quantity = draftMap[product.Id];
     });
 
     this.selectedProducts.forEach((product) => {
@@ -226,71 +223,113 @@ export default class E_Commerce extends LightningElement {
     this.totalPriceSum = this.finalSelectedProducts.reduce(
       (sum, product) => sum + product.TotalPrice,
       .0
-    );   
+    );
   }
 
+  //....................ORDER LAYOUT VALUE....................
+  @wire(getRecord, { recordId: '$order.AccountId', fields: [ACCOUNT_NAME_FIELD] })
+  account;
 
+  @wire(getRecord, { recordId: '$order.CustomerAuthorizedById', fields: [AUTHORIZED_BY_ID] })
+  contact;
+
+  @wire(getRecord, { recordId: '$order.ContractId', fields: [CONTRACT_NUMBER_FIELD] })
+  contractRecord;
+
+  @wire(getRecord, { recordId: '$order.Pricebook2Id', fields: [PRICEBOOK_NAME_FIELD] })
+  pricebookRecord;
+
+  get accountName() {
+    return getFieldValue(this.account.data, ACCOUNT_NAME_FIELD);
+  }
+  get CustomerAuthorizedById() {
+    return getFieldValue(this.contact.data, AUTHORIZED_BY_ID);
+  }
+
+  get contractNumber() {
+    return getFieldValue(this.contractRecord.data, CONTRACT_NUMBER_FIELD);
+  }
+
+  get pricebookName() {
+    return getFieldValue(this.pricebookRecord.data, PRICEBOOK_NAME_FIELD);
+  }
+
+  //....................VALIDATIONS....................
   validateProductDetails() {
-      return true;
-    }
-
-  validateSelectedProducts() {
-    if (this.selectedProducts.length === 0) {
-    // for (let i = 0; i < this.selectedProducts.length; i++) {
-    //   if (this.selectedProducts[i].Quantity <= 0) {
-        return false;
-      }
-    // }
     return true;
   }
 
-
-  handleAddToCart() {       
-
-    createOrderRecord({ order: this.order})
-    .then((result) => {
-      const orderId = result;
-      this.showToast('Success', 'Order created successfully', 'success');
-
-    const pricebookId = this.order.Pricebook2Id;
-    const productIds = this.finalSelectedProducts.map((product) => product.Id);
-    const quantities = this.finalSelectedProducts.map((product) => product.Quantity);
-
-    createOrderProducts({ orderId, productIds, quantities, pricebookId })
-    .then(() => {
-          this.showToast(
-            'Success',
-            'Order products created successfully',
-            'success'
-          );
-        })
-        .catch((error) => {
-          this.showToast(
-            'Error',
-            'An error occurred while trying to create order products',
-            'error'
-          );
-          console.error('Error creating order products:', error);
-        });
-    })
-    .catch((error) => {
-      this.showToast(
-        'Error',
-        'An error occurred while trying to create the Order record',
-        'error'
-      );
-      console.error('Error creating Order:', error);
-    });
-
+  validateSelectedProducts() {
+    if (this.selectedProducts.length === 0) {
+      return false;
+    }
+    return true;
   }
 
-  showToast(title, message, variant) {
-      const event = new ShowToastEvent({
-          title: title,
-          message: message,
-          variant: variant
+  //....................ADD TO CARD....................
+  handleAddToCart() {
+
+    createOrderRecord({ order: this.order })
+      .then((result) => {
+        const orderId = result;
+        this.showToast('Success', 'Order created successfully', 'success');
+
+        const pricebookId = this.order.Pricebook2Id;
+        const productIds = this.finalSelectedProducts.map((product) => product.Id);
+        const quantities = this.finalSelectedProducts.map((product) => product.Quantity);
+
+        createOrderProducts({ orderId, productIds, quantities, pricebookId })
+          .then(() => {
+            this.showToast(
+              'Success',
+              'Order products created successfully',
+              'success'
+            );
+            this.showPreviewCard = false;
+            this.showOrderCard = true;
+          })
+          .catch((error) => {
+            this.showToast(
+              'Error',
+              'An error occurred while trying to create order products',
+              'error'
+            );
+            console.error('Error creating order products:', error);
+          });
+      })
+      .catch((error) => {
+        this.showToast(
+          'Error',
+          'An error occurred while trying to create the Order record',
+          'error'
+        );
+        console.error('Error creating Order:', error);
       });
-      this.dispatchEvent(event);
-  } 
+  }
+
+  //....................CANCLE BUTTON....................
+  handleCancle() {
+    this.showPreviewCard = false;
+    this.showOrderCard = true;
+    
+    // Reset order object and other relevant variables
+    this.order = {
+        Status: 'Draft'
+    };
+    this.selectedProducts = [];
+    this.finalSelectedProducts = [];
+    this.draftValues = [];
+    this.totalPriceSum = 0;
+}
+
+  //....................SHOW TOAST....................
+  showToast(title, message, variant) {
+    const event = new ShowToastEvent({
+      title: title,
+      message: message,
+      variant: variant
+    });
+    this.dispatchEvent(event);
+  }
 
 }
